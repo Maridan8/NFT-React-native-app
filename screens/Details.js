55cb65aeca95b337b7b9a3b8aba6bbfabbbe2505
Reinhardt Lagat <56647015+Reinhardt254@users.noreply.github.com>
2023-12-13 11:59:18 +0300

    first commit

---
+++ screens/Details.js
@@ -0,0 +1,31 @@
+import {Text, View, SafeAreaView, Image, StatusBar, FlatList} from 'react-native'
+import {COLORS, SIZES, SHADOWS, FONTS, assets} from "../constants"
+import {circleButton, RectButton, SubInfo, FocusedStatusBar, DetailsBid, DetailsDesc} from "../components"
+
+const Details = ({route, navigation}) => {
+  return (
+    <Text>Details</Text>
+  )
+};
+
+export default Details
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
